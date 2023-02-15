3bfb95c948e46865808dbd8ba4b05544220ea0b6
Simon <121458315+PerfectPurplee@users.noreply.github.com>
2023-02-15 20:12:14 +0100

    improving a window settings, added container to a window

--- Main/Gamepanel.java
+++ Main/Gamepanel.java
@@ -1,4 +1,23 @@
 package Main;
 
-public class Gamepanel extends Main {
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class Gamepanel extends JPanel {
+
+    final int tile = 16;
+    final int scale = 3;
+    final int tileSize = tile * scale;
+    final int maxColumns = 12;
+    final int maxRows = 16;
+
+    final int screenWidth = tileSize * maxRows;
+    final int screenHeight = tileSize * maxColumns;
+
+    Gamepanel() {
+       this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+       this.setBackground(Color.BLACK);
+       this.setDoubleBuffered(true);
+    }
 }
--- Main/Main.java
+++ Main/Main.java
@@ -7,12 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+//      Creates a frame(window) for a game, and then adds a container(gamepanel)
+//      window size is set to a size of a gamepanel by a class method frame.pack.
         JFrame frame = new JFrame("MY 2D GAME ADVENTURE");
+        Gamepanel gamePanel = new Gamepanel();
 
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+
+        frame.add(gamePanel);
+        frame.pack();
+
 
 
     }
